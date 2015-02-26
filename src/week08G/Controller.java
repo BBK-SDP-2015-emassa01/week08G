61904d4896e238e6874281512a13b8f3395447ff
Esha Massand <ubemas001@mail.bbk.ac.uk>
2015-02-26 21:08:00 +0000

    Class notes on Controllers

---
+++ src/week08G/Controller.java
@@ -0,0 +1,8 @@
+package week08G;
+
+public class Controller {
+	/*
+	 * Java doesnt model MVC but instead delagate, which means they are really tightly coupled that you can't pull them apart.
+	 */
+
+}
